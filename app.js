150089ceea9cd51c712de53d79b709c93afa9ac9
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2022-02-20 15:52:22 +0000

    remove

---
+++ app.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.button');
+const drawer = document.querySelector('.drawer');
+
+//Hidden class , calling out by javascript
+const newButon = document.querySelector('.newButton');
+const newDrawer = document.querySelector('.new-drawer');
+
+const drawerControl = () => {};
